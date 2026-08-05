9be32cf9cba7fdc92ca8825f0296b33f711fc351
Sami <samivijarra1@gmail.com>
2026-08-05 01:46:05 -0300

    feat: define CreateExerciseDto with validation rules and update Exercise entity to include createdBy relation

--- src/exercises/dto/create-exercise.dto.ts
+++ src/exercises/dto/create-exercise.dto.ts
@@ -1,1 +1,25 @@
-export class CreateExerciseDto {}
+import { IsArray, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class CreateExerciseDto {
+  @IsString()
+  @MinLength(4)
+  name!: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  primaryMuscles!: string[];
+
+  @IsString()
+  @IsOptional()
+  equipment?: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  instructions?: string[];
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  images?: string[];
+}
--- src/exercises/entities/exercise.entity.ts
+++ src/exercises/entities/exercise.entity.ts
@@ -1,5 +1,12 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { ExerciseImage } from './exercise-image.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Entity('exercises')
 export class Exercise {
@@ -37,4 +44,10 @@ export class Exercise {
     cascade: true,
   })
   images?: ExerciseImage[];
+
+  @ManyToOne(() => User, {
+    nullable: true,
+    onDelete: 'SET NULL',
+  })
+  createdBy?: User;
 }
