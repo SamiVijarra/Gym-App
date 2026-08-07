b3928925883a22ddcbbb40329fd76332e5e2173f
Sami <samivijarra1@gmail.com>
2026-08-07 00:21:05 -0300

    fix: update package dependencies and improve module imports
    - Downgraded @nestjs/typeorm from ^11.0.3 to 11.0.2 for compatibility.
    - Downgraded typeorm from ^1.1.0 to ^0.3.20 to align with the new @nestjs/typeorm version.
    - Added class-transformer as a new dependency.
    - Reorganized imports in app.module.ts for better readability.
    - Updated exercises.module.ts to include TypeOrmModule for Exercise and ExerciseImage entities, and exported ExercisesService for use in other modules.

--- src/exercises/exercises.module.ts
+++ src/exercises/exercises.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ExercisesService } from './exercises.service';
 import { ExercisesController } from './exercises.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Exercise, ExerciseImage } from './entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Exercise, ExerciseImage])],
   controllers: [ExercisesController],
   providers: [ExercisesService],
+  exports: [ExercisesService],
 })
 export class ExercisesModule {}
