f900918acbf5f69c56419814624f0b0b8fc4151c
Sami <samivijarra1@gmail.com>
2026-08-06 22:47:13 -0300

    feat: add FindExercisesDto for filtering in findAll method and update controller methods to include user validation

--- src/exercises/dto/index.ts
+++ src/exercises/dto/index.ts
@@ -1,2 +1,3 @@
 export * from './create-exercise.dto';
 export * from './update-exercise.dto';
+export * from './find.exercise.dto';
--- src/exercises/exercises.controller.ts
+++ src/exercises/exercises.controller.ts
@@ -6,40 +6,48 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Query,
+  ParseUUIDPipe,
 } from '@nestjs/common';
-import { ExercisesService } from './exercises.service';
-import { CreateExerciseDto } from './dto/create-exercise.dto';
-import { UpdateExerciseDto } from './dto/update-exercise.dto';
+import { AuthGuard } from '@nestjs/passport';
 
+import { ExercisesService } from './exercises.service';
+import { CreateExerciseDto, UpdateExerciseDto, FindExercisesDto } from './dto';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+
+@UseGuards(AuthGuard('jwt'))
 @Controller('exercises')
 export class ExercisesController {
   constructor(private readonly exercisesService: ExercisesService) {}
 
-  @Post()
-  create(@Body() createExerciseDto: CreateExerciseDto) {
-    return this.exercisesService.create(createExerciseDto);
-  }
-
   @Get()
-  findAll() {
-    return this.exercisesService.findAll();
+  findAll(@Query() findExercisesDto: FindExercisesDto) {
+    return this.exercisesService.findAll(findExercisesDto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.exercisesService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.exercisesService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createExerciseDto: CreateExerciseDto, @GetUser() user: User) {
+    return this.exercisesService.create(createExerciseDto, user);
   }
 
   @Patch(':id')
   update(
-    @Param('id') id: string,
+    @Param('id', ParseUUIDPipe) id: string,
     @Body() updateExerciseDto: UpdateExerciseDto,
+    @GetUser() user: User,
   ) {
-    return this.exercisesService.update(+id, updateExerciseDto);
+    return this.exercisesService.update(id, updateExerciseDto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.exercisesService.remove(+id);
+  remove(@Param('id', ParseUUIDPipe) id: string, @GetUser() user: User) {
+    return this.exercisesService.remove(id, user);
   }
 }
