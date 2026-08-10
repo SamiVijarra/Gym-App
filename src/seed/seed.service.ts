2d743001dfbaffdaf4562e3a69686cea78e9257c
Sami <samivijarra1@gmail.com>
2026-08-10 18:32:08 -0300

    feat: refactor SeedService to include RawExercise interface and remove unused methods

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Exercise, ExerciseImage } from 'src/exercises/entities';
+import { Repository } from 'typeorm';
 
+interface RawExercise {
+  id: string;
+  name: string;
+  primaryMuscles: string[];
+  equipment: string | null;
+  instructions: string[];
+  images: string[];
+}
 @Injectable()
 export class SeedService {
-  create() {
-    return 'This action adds a new seed';
-  }
+  constructor(
+    @InjectRepository(Exercise)
+    private readonly exerciseRepository: Repository<Exercise>,
+    @InjectRepository(ExerciseImage)
+    private readonly exerciseImageRepository: Repository<ExerciseImage>,
+  ) {}
 
-  findAll() {
-    return `This action returns all seed`;
-  }
+  async seedExercises() {}
 
-  findOne(id: number) {
-    return `This action returns a #${id} seed`;
-  }
-
-  update(id: number) {
-    return `This action updates a #${id} seed`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} seed`;
+  private transformExercise(item: RawExercise) {
+    return {
+      name: item.name,
+      primaryMuscles: item.primaryMuscles,
+      equipment: item.equipment ?? undefined,
+      instructions: item.instructions,
+      sourceId: item.id,
+      images: item.images.map((path) =>
+        this.exerciseImageRepository.create({
+          url: `https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/exercises/${path}`,
+        }),
+      ),
+    };
   }
 }
