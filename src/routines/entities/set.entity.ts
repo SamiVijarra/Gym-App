38b421b89e8a922a85b136127cbb84b1312d459f
Sami <samivijarra1@gmail.com>
2026-08-10 20:59:04 -0300

    feat: implement Set entity with properties and relationships to RoutineExercise

--- src/routines/entities/set.entity.ts
+++ src/routines/entities/set.entity.ts
@@ -1,1 +1,28 @@
-export class Set {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { RoutineExercise } from './routine-exercise.entity';
+
+@Entity('sets')
+export class Set {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @ManyToOne(() => RoutineExercise, (routineExercise) => routineExercise.sets, {
+    onDelete: 'CASCADE',
+  })
+  routineExercise!: RoutineExercise;
+
+  @Column('int')
+  order!: number;
+
+  @Column('numeric', { precision: 10, scale: 2 })
+  weight!: number;
+
+  @Column('int')
+  reps!: number;
+
+  @Column('int', { nullable: true })
+  restSeconds?: number;
+
+  @Column('text', { nullable: true })
+  notes?: string;
+}
