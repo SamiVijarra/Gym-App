33a7e278dc7472bb46f6f6d4f4c59cd1de3403cd
Sami <samivijarra1@gmail.com>
2026-08-11 20:53:31 -0300

    feat: refactor RoutinesController and RoutinesService to improve structure and add user authentication

--- src/routines/routines.controller.ts
+++ src/routines/routines.controller.ts
@@ -1,15 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { RoutinesService } from './routines.service';
-import { CreateRoutineDto } from './dto/create-routine-day.dto';
-import { UpdateRoutineDto } from './dto/update-routine-day.dto';
+import { CreateRoutineDayDto, UpdateRoutineDayDto } from './dto';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { User } from 'src/users/entities/user.entity';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('routines')
 export class RoutinesController {
   constructor(private readonly routinesService: RoutinesService) {}
 
-  @Post()
-  create(@Body() createRoutineDto: CreateRoutineDto) {
-    return this.routinesService.create(createRoutineDto);
+  @Post('days')
+  create(
+    @Body() createRoutineDayDto: CreateRoutineDayDto,
+    @GetUser() user: User,
+  ) {
+    return this.routinesService.createDay(createRoutineDayDto, user);
   }
 
   @Get()
@@ -18,13 +33,16 @@ export class RoutinesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findMy(@Param('id') id: string) {
     return this.routinesService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoutineDto: UpdateRoutineDto) {
-    return this.routinesService.update(+id, updateRoutineDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateRoutineDayDto: UpdateRoutineDayDto,
+  ) {
+    return this.routinesService.updateDay(+id, updateRoutineDayDto);
   }
 
   @Delete(':id')
--- src/routines/routines.service.ts
+++ src/routines/routines.service.ts
@@ -3,11 +3,13 @@ import {
   Injectable,
   InternalServerErrorException,
 } from '@nestjs/common';
+
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 import { CreateRoutineDayDto, UpdateRoutineDayDto } from './dto';
 import { User } from 'src/users/entities/user.entity';
-import { InjectRepository } from '@nestjs/typeorm';
 import { RoutineDay, RoutineExercise, Set } from './entities';
-import { Repository } from 'typeorm';
 
 interface PostgresError {
   code: string;
@@ -36,21 +38,43 @@ export class RoutinesService {
     }
   }
 
-  findAll() {
+  findMyRoutine(user: User) {
     return `This action returns all routines`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} routine`;
-  }
-
-  update(id: number, updateRoutineDayDto: UpdateRoutineDayDto) {
+  updateDay(id: number, updateRoutineDayDto: UpdateRoutineDayDto) {
     return `This action updates a #${id} routine`;
   }
 
-  remove(id: number) {
+  removeDay(id: number) {
     return `This action removes a #${id} routine`;
   }
+
+  addExerciseToDay(
+    dayId: string,
+    createRoutineDatDto: CreateRoutineDayDto,
+    user: User,
+  ) {
+    return `This action returns a #${dayId} routine`;
+  }
+
+  removeExerciseFromDay(id: number, user: User) {
+    return;
+  }
+
+  addSet(
+    routineExerciseId: RoutineExercise,
+    createRoutineDatDto: CreateRoutineDayDto,
+    user: User,
+  ) {}
+
+  updateSet(
+    routineExerciseId: RoutineExercise,
+    createRoutineDatDto: CreateRoutineDayDto,
+    user: User,
+  ) {}
+
+  removeSet(routineExerciseId: RoutineExercise, user: User) {}
 
   private handleDBErrors(error: unknown): never {
     const pgError = error as PostgresError;
