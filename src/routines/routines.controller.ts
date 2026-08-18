f5eee0c10651683fd2013b8a8dba11195a7c2321
Sami <samivijarra1@gmail.com>
2026-08-18 20:21:41 -0300

    feat: enhance RoutinesController with exercise and set management for routine days

--- src/routines/routines.controller.ts
+++ src/routines/routines.controller.ts
@@ -7,9 +7,17 @@ import {
   Param,
   Delete,
   UseGuards,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { RoutinesService } from './routines.service';
-import { CreateRoutineDayDto, UpdateRoutineDayDto } from './dto';
+import {
+  CreateRoutineDayDto,
+  CreateRoutineExerciseDto,
+  CreateSetDto,
+  UpdateRoutineDayDto,
+  UpdateRoutineExerciseDto,
+  UpdateSetDto,
+} from './dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/decorators/get-user.decorator';
 import { User } from 'src/users/entities/user.entity';
@@ -20,7 +28,7 @@ export class RoutinesController {
   constructor(private readonly routinesService: RoutinesService) {}
 
   @Post('days')
-  create(
+  createDay(
     @Body() createRoutineDayDto: CreateRoutineDayDto,
     @GetUser() user: User,
   ) {
@@ -34,7 +42,7 @@ export class RoutinesController {
 
   @Patch('days/:id')
   updatedDay(
-    @Param('id') id: string,
+    @Param('id', ParseUUIDPipe) id: string,
     @Body() updateRoutineDayDto: UpdateRoutineDayDto,
     @GetUser() user: User,
   ) {
@@ -42,7 +50,64 @@ export class RoutinesController {
   }
 
   @Delete('days/:id')
-  removeDay(@Param('id') id: string, @GetUser() user: User) {
+  removeDay(@Param('id', ParseUUIDPipe) id: string, @GetUser() user: User) {
     return this.routinesService.removeDay(id, user);
+  }
+
+  @Post('days/:dayId/exercises')
+  addExerciseToDay(
+    @Param('dayId', ParseUUIDPipe) dayId: string,
+    @Body() createRoutineExerciseDto: CreateRoutineExerciseDto,
+    @GetUser() user: User,
+  ) {
+    return this.routinesService.addExerciseToDay(
+      dayId,
+      createRoutineExerciseDto,
+      user,
+    );
+  }
+
+  @Patch('exercises/:id')
+  updatedExerciseInDay(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateRoutineExerciseDto: UpdateRoutineExerciseDto,
+    @GetUser() user: User,
+  ) {
+    return this.routinesService.updateExerciseInDay(
+      id,
+      updateRoutineExerciseDto,
+      user,
+    );
+  }
+
+  @Delete('exercises/:id')
+  removeExerciseFromDay(
+    @Param('id', ParseUUIDPipe) id: string,
+    @GetUser() user: User,
+  ) {
+    return this.routinesService.removeExerciseFromDay(id, user);
+  }
+
+  @Post('exercises/:id/sets')
+  addSet(
+    @Param('id', ParseUUIDPipe) routineExerciseId: string,
+    @Body() createSetDto: CreateSetDto,
+    @GetUser() user: User,
+  ) {
+    return this.routinesService.addSet(routineExerciseId, createSetDto, user);
+  }
+
+  @Patch('sets/:id')
+  updatedSet(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateSetDto: UpdateSetDto,
+    @GetUser() user: User,
+  ) {
+    return this.routinesService.updateSet(id, updateSetDto, user);
+  }
+
+  @Delete('sets/:is')
+  removeSet(@Param('id', ParseUUIDPipe) id: string, @GetUser() user: User) {
+    return this.routinesService.removeSet(id, user);
   }
 }
