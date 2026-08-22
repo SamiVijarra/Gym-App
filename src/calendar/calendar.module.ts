a88f2ebeccace37f2ab9af26291d6f53cd470c67
Sami <samivijarra1@gmail.com>
2026-08-22 00:45:40 -0300

    feat: add TypeOrm integration for calendar entities in CalendarModule

--- src/calendar/calendar.module.ts
+++ src/calendar/calendar.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CalendarService } from './calendar.service';
 import { CalendarController } from './calendar.controller';
+import { ExercisesModule } from 'src/exercises/exercises.module';
+import { RoutinesModule } from 'src/routines/routines.module';
+
+import {
+  CalendarEntry,
+  HistoryEntry,
+  HistoryExercise,
+  HistorySet,
+} from './entities';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      CalendarEntry,
+      HistoryEntry,
+      HistoryExercise,
+      HistorySet,
+    ]),
+    RoutinesModule,
+    ExercisesModule,
+  ],
   controllers: [CalendarController],
   providers: [CalendarService],
 })
---
+++ src/calendar/entities/index.ts
@@ -0,0 +1,4 @@
+export * from './calendar-entry.entity';
+export * from './history-entry.entity';
+export * from './history-exercise.entity';
+export * from './history-set.entity';
