01d6cd706cf15f49f0ece457b4d6c64fe9a2685d
Sami <samivijarra1@gmail.com>
2026-09-04 21:14:10 -0300

    feat: add CalendarEntry to TypeOrmModule imports in RoutinesModule

--- src/routines/routines.module.ts
+++ src/routines/routines.module.ts
@@ -4,10 +4,11 @@ import { RoutinesController } from './routines.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoutineDay, RoutineExercise, Set } from './entities';
 import { ExercisesModule } from 'src/exercises/exercises.module';
+import { CalendarEntry } from 'src/calendar/entities';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([RoutineDay, RoutineExercise, Set]),
+    TypeOrmModule.forFeature([RoutineDay, RoutineExercise, Set, CalendarEntry]),
     ExercisesModule,
   ],
   controllers: [RoutinesController],
